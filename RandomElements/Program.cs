c26f890e143c94636d9afb7b85421b9a370dd874
Sigrit <sigrit.sults@tktk.ee>
2022-11-24 16:57:46 +0200

    Week6Arrays

---
+++ RandomElements/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takaeaway" };
+
+Random rnd= new Random();
+
+int RandomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat: {snacks[RandomIndex]}");
